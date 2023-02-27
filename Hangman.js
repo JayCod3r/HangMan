97933b8bb7c57d73bc3a35871aa76d579550b849
guro1881 <casiopains@gmail.com>
2023-02-27 21:11:33 +1100

    added some stuff for checking if gameover and counting fails.

--- Hangman.js
+++ Hangman.js
@@ -1,8 +1,69 @@
 const VALIDKEYS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var words = [
+    "ablaze",
+    "afflict",
+    "baggage",
+    "baffling",
+    "bewitch",
+    "blunder",
+    "brawny",
+    "cater",
+    "chaotic",
+    "coastal",
+    "collide",
+    "crimson",
+    "debris",
+    "defy",
+    "dazzling",
+    "deliberate",
+    "detest",
+    "dignity",
+    "dismantle",
+    "eccentric",
+    "ecstatic",
+    "eloquent",
+    "embark",
+    "enrage",
+    "epitome",
+    "exhale",
+    "fascinate",
+    "fidget",
+    "flimsy",
+    "frantic",
+    "grueling",
+    "hazy",
+    "humble",
+    "ignite",
+    "impartial",
+    "indulge",
+    "infinite",
+    "insight",
+    "irksome",
+    "jagged",
+    "jovial",
+    "keen",
+    "knack",
+    "languid",
+    "lethal",
+    "lucid",
+    "luminous",
+    "marvel",
+    "meander",
+    "meticulous",
+    "momentum",
+    "muddle"
+];
+
+
 var strokedKeys = [];
 var word = ['n', 'u', 'l', 'l'];
 var hiddenWord = word;
 var failCount = 0;
+var failed = false;
+
+function pickRandomWord() {
+    word = words[Math.floor(Math.random() * 50)];
+}
 
 function HideWord() {
     var tempString = [];
@@ -15,10 +76,19 @@ function HideWord() {
 }
 
 function UnHideWord() {
+    let fail = true;
     for (let i = 0; i < word.length; i++) {
         if (strokedKeys.includes(word[i])) {
             hiddenWord[i] = word[i];
         }
+        if (strokedKeys[strokedKeys.length - 1] == word[i]) {
+            fail = false;
+        }
+    }
+    console.log(fail);
+    if (fail) {
+        failCount++;
+        console.log(failCount);
     }
 }
 
@@ -29,10 +99,18 @@ function PushWordToId() {
 function DebugTestKeyStrokes() {
     document.addEventListener('keydown', (event) => {
         var name = event.key;
-        if (VALIDKEYS.includes(name)) {
-            strokedKeys.push(name);
-            UnHideWord();
-            PushWordToId();
+        if (failCount > 9) {
+            alert("Fail word is " + word);
+        } else {
+            if (VALIDKEYS.includes(name)) {
+                if (!strokedKeys.includes(name)) {
+                    strokedKeys.push(name);
+                }
+                UnHideWord();
+                PushWordToId();
+
+            }
         }
+
     }, false);
 }
